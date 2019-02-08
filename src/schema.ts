f2870b22326c2a58f7b406f6775759a01b4d5420
Kythyria Tieran <kythyria@berigora.net>
2019-02-08 17:21:11 +0000

    Explain the schema a little.

--- src/schema.ts
+++ src/schema.ts
@@ -1,9 +1,25 @@
-export type Content = "cdata" | "pcdata" | "any" | "empty" | DtdElement[];
+// "cdata" : Things which aren't tags (urls, emotes, text)
+// "pcdata" : PLain text only, even if it's a tag.
+// "any" : anything
+// "empty" : not even a closing tag (eg, [hr])
+export type Content = ("cdata" | "pcdata" | "any" | "empty" | DtdElement)[];
 
 export interface DtdElement {
+    // name used in bbcode
     tagname : string;
+    
+    // What can appear inside the element
     contentModel : Content;
+
+    // If true, [foo][foo] is read like [foo][/foo][foo].
     isDelimiter: boolean;
+
+    // If true, the attribute MUST be present. If false, MUST NOT.
+    // If "optional", is OPTIONAL (eg, [url])
     hasAttribute : boolean | "optional";
-    component : string;
+
+    // If you're using React for the "to HTML" step, this is a good place
+    // to put the component to use. `any` because the actual signature
+    // would require an otherwise unnecessary React dependency.
+    component : any;
 }
